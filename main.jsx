f69df8cbe7dbf55e9987355f85187ded0e97c270
Malay-Sharma <malaysharma0001@gmail.com>
2025-07-03 02:25:34 +0530

    restructured the app

--- main.jsx
+++ main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import './src/index.css'
+import App from './src/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
